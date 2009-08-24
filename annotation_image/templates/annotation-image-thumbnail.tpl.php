0c31fac54df35394be001a9a84e8c8ffefc022b4
Neil Drumm <drumm@3064.no-reply.drupal.org>
2009-08-24 07:43:57 +0000

    Make thumbnails into links

--- annotation_image/templates/annotation-image-thumbnail.tpl.php
+++ annotation_image/templates/annotation-image-thumbnail.tpl.php
@@ -1,4 +1,4 @@
-<div class="annotation-image-thumbnail-outer" style="background: url(<?php print $back; ?>); width: <?php print $width; ?>px; height: <?php print $height; ?>px;">
-  <div class="annotation-image-thumbnail-inner" style="background: url(<?php print $front; ?>) -<?php print $annotation_top; ?>px -<?php print $annotation_top; ?>px; width: <?php print $annotation_width ?>px; height: <?php print $annotation_height; ?>px; top: <?php print $annotation_top; ?>px; left: <?php print $annotation_left; ?>px;">
-  </div>
-</div>
+<a href="#annotation-cid-<?php print $comment->cid; ?>" class="annotation-link annotation-image-thumbnail-outer" style="background: url(<?php print $back; ?>); width: <?php print $width; ?>px; height: <?php print $height; ?>px;">
+  <span class="annotation-image-thumbnail-inner" style="background: url(<?php print $front; ?>) -<?php print $annotation_top; ?>px -<?php print $annotation_top; ?>px; width: <?php print $annotation_width ?>px; height: <?php print $annotation_height; ?>px; top: <?php print $annotation_top; ?>px; left: <?php print $annotation_left; ?>px;">
+  </span>
+</a>
