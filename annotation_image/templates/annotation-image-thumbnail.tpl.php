2afeef20507943bee2a83f725f7b246348c97069
Neil Drumm <drumm@3064.no-reply.drupal.org>
2009-08-24 07:38:42 +0000

    Add thumbnails to image annotation comments.

---
+++ annotation_image/templates/annotation-image-thumbnail.tpl.php
@@ -0,0 +1,4 @@
+<div class="annotation-image-thumbnail-outer" style="background: url(<?php print $back; ?>); width: <?php print $width; ?>px; height: <?php print $height; ?>px;">
+  <div class="annotation-image-thumbnail-inner" style="background: url(<?php print $front; ?>) -<?php print $annotation_top; ?>px -<?php print $annotation_top; ?>px; width: <?php print $annotation_width ?>px; height: <?php print $annotation_height; ?>px; top: <?php print $annotation_top; ?>px; left: <?php print $annotation_left; ?>px;">
+  </div>
+</div>
