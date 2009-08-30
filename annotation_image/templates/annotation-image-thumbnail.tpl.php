e3870354fbeab8d994fb77d87a4bea3a50876bd4
Neil Drumm <drumm@3064.no-reply.drupal.org>
2009-08-30 02:05:50 +0000

    Improve and simplify thumbnails

--- annotation_image/templates/annotation-image-thumbnail.tpl.php
+++ annotation_image/templates/annotation-image-thumbnail.tpl.php
@@ -1,4 +1,4 @@
-<a href="#annotation-cid-<?php print $comment->cid; ?>" class="annotation-link annotation-image-thumbnail-outer" style="background: url(<?php print $back; ?>); width: <?php print $width; ?>px; height: <?php print $height; ?>px;">
-  <span class="annotation-image-thumbnail-inner" style="background: url(<?php print $front; ?>) -<?php print $annotation_left; ?>px -<?php print $annotation_top; ?>px; width: <?php print $annotation_width ?>px; height: <?php print $annotation_height; ?>px; top: <?php print $annotation_top; ?>px; left: <?php print $annotation_left; ?>px;">
-  </span>
+<a href="#annotation-cid-<?php print $comment->cid; ?>" class="annotation-link annotation-image-thumbnail" style="width: <?php print $width; ?>px; height: <?php print $height; ?>px;">
+  <span class="back" style="background: url(<?php print $thumbnail; ?>)"></span>
+  <span class="front" style="background: url(<?php print $thumbnail; ?>) -<?php print $annotation_left; ?>px -<?php print $annotation_top; ?>px; width: <?php print $annotation_width ?>px; height: <?php print $annotation_height; ?>px; top: <?php print $annotation_top; ?>px; left: <?php print $annotation_left; ?>px;"></span>
 </a>
