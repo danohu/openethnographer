c461898c76b4844bf4e379c07d1d6bba1cb40af4
Liang Shen <shenzhuxi@gmail.com>
2013-05-01 17:00:02 +0100

    the first 7.x-1.x commit port from https://github.com/sanduhrs/drupal-annotator/tree/7.x-1.x/modules/annotation

--- annotation_image/templates/annotation-image-thumbnail.tpl.php
+++
@@ -1,4 +0,0 @@
-<a href="#annotation-cid-<?php print $comment->cid; ?>" class="annotation-link annotation-image-thumbnail" style="width: <?php print $width; ?>px; height: <?php print $height; ?>px;">
-  <span class="back" style="background: url(<?php print $thumbnail; ?>)"></span>
-  <span class="front" style="background: url(<?php print $thumbnail; ?>) -<?php print $annotation_left; ?>px -<?php print $annotation_top; ?>px; width: <?php print $annotation_width ?>px; height: <?php print $annotation_height; ?>px; top: <?php print $annotation_top; ?>px; left: <?php print $annotation_left; ?>px;"></span>
-</a>
--- templates/annotation_comment.tpl.php
+++
@@ -1,5 +0,0 @@
-<div class="annotation-comment">
-  <div class="info"><strong><?php print $name ?></strong> at <?php print $time ?></div>
-  <?php print $body ?>
-  <?php print $links ?>
-</div>
