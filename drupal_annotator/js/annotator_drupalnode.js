76b75b4064bd3a4287a096a453bc94a7fb756da5
Dan O'Huiginn <daniel@ohuiginn.net>
2014-12-19 16:08:13 +0100

    Fix DrupalNode javascript
    Add some javascript omitted from the last commit

--- drupal_annotator/js/annotator_drupalnode.js
+++ drupal_annotator/js/annotator_drupalnode.js
@@ -1,30 +1,41 @@
 
+var _ref,
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
 Annotator.Plugin.DrupalNode = (function(_super) {
-   function DrupalNode(element) {
-	this.element = element;
-	this.nid = jQuery(element).parents('.node').attr('about').split('/').slice(-1).pop();
-	this.pluginSubmit = __bind(this.pluginSubmit, this);
+      __extends(DrupalNode, _super);
+    function DrupalNode(element) {
+        this.element = element;
+        this.nid = jQuery(element).parents('.node').attr('about').split('/').slice(-1).pop();
+        this.pluginSubmit = __bind(this.pluginSubmit, this);
       return DrupalNode.__super__.constructor.apply(this, arguments);
-    }
+    } 
 
       
     DrupalNode.prototype.pluginSubmit = function(field, annotation){
-	annotation.uri = window.location.origin + '/node/' + this.nid;      
-	return annotation.nid = this.nid;
-	};
+        annotation.uri = window.location.origin + '/node/' + this.nid;      
+        return annotation.nid = this.nid;
+        };
 
     DrupalNode.prototype.pluginInit = function() {
       if (!Annotator.supported()) {
         return;
       }
 
-	this.field = this.annotator.editor.addField({
-	  submit: this.pluginSubmit,
+        this.field = this.annotator.editor.addField({
+          submit: this.pluginSubmit,
       });
     };
-
-
-    _super.DrupalNode = DrupalNode;
     return DrupalNode;
 
   })(Annotator.Plugin);
+
+(function ($) {
+  Drupal.behaviors.annotatorTags = {
+    attach: function (context, settings) {
+      Drupal.Annotator.annotator('addPlugin', 'DrupalNode');
+    }
+  };
+})(jQuery);
