fc122966acb7d7aeb0016eacc4008800574c4a12
Dan O'Huiginn <daniel@ohuiginn.net>
2015-01-03 16:31:43 +0100

    Prevent race condition in attaching autocomplete code
    Add autocomplete to the Tag plugin as an option when activating the plugin,
    rather than manually bolting it on later.
    
    This solves the issue noted by @tanius at https://github.com/edgeryders/openethnographer/pull/3/#commitcomment-9072160

--- drupal_annotator/js/annotator_tags.js
+++ drupal_annotator/js/annotator_tags.js
@@ -1,17 +1,16 @@
 (function ($) {
   Drupal.behaviors.annotatorTags = {
     attach: function (context, settings) {
-      Drupal.Annotator.annotator('addPlugin', 'Tags');
+      Drupal.Annotator.annotator('addPlugin', 'Tags', {
+        onInit: function(that){
+	  jQuery(that.field).find(':input').autocomplete({
+ 	      source: Drupal.settings.autocomplete_terms,
+	      delay: 0,
+	      position: { my : "right top", at: "left bottom" },
+	  });
+	}
+
+	});
     }
   };
 })(jQuery);
-
-jQuery(document).ready(function(){
-    for(i in Annotator._instances){
-	Annotator._instances[i].plugins.Tags.input.autocomplete({
- 	 source: Drupal.settings.autocomplete_terms,
-	 delay: 0,
-	 position: { my : "right top", at: "left bottom" },
-    });
-	}
-});
